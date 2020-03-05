48744fec75a5173af0991adb53f93864603cc8fd
liten-kersti <tamiteatime@gmail.com>
2020-03-05 10:58:15 +0200

    setting project

--- src/test/java/runners/CucumberRunner.java
+++ src/test/java/runners/CucumberRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         plugin = {"pretty", "html:cucumber-report/html-report",
                 "junit:cucumber-report/junit-report.xml",
                 "json:cucumber-report/json-report.json"},
-        tags = {"~@test", "~@bug"},
+        tags = {"~@test", "~@bug", "~@bug2"},
         glue = {"stepDefinitions"}
 )
 public class CucumberRunner {
