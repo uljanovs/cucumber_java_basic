c7be8251fdc9a6b861bb7a07a0e0e02ddd53f2e5
KonstantsiyaP <kasya.aysak@gmail.com>
2020-03-05 16:54:39 +0200

    SampleSteps

--- src/test/java/stepDefinitions/SampleSteps.java
+++ src/test/java/stepDefinitions/SampleSteps.java
@@ -100,4 +100,46 @@ public class SampleSteps {
     public void iAmOnActionPage() {
         driver.get("https://uljanovs.github.io/site/examples/actions");
     }
+
+    @Given("^I should see home page description like my wife$")
+    public void i_should_see_home_page_description_like_my_wife() throws Throwable {
+        driver.get("https://uljanovs.github.io/site/examples/age");
+
+    }
+
+    @And("^I should see something$")
+    public void i_should_see_something() throws Throwable {
+        driver.get("https://uljanovs.github.io/site/examples/age");
+
+    }
+
+    ///ACTIVITY_2
+
+   /*@Given("^I am on page$")
+    public void i_am_on_page() {
+        driver.get("https://www.google.lv/");
+    }
+
+   @Given("^I see search field$")
+    public void i_see_search_field() throws Throwable {
+        assertTrue(driver.findElement(By.name("q")).isDisplayed());
+    }
+
+    @When("^I input cats$")
+    public void i_input_cats() throws Throwable {
+        WebElement name = driver.findElement(By.name("q"));
+        name.sendKeys("Cats");
+    }
+
+    @When("^I click search button$")
+    public void i_click_search_button() throws Throwable {
+        WebElement button12 = driver.findElement(By.name("btnK"));
+        button12.click();
+    }
+
+    @Then("^I see request result cats in search field$")
+    public void i_see_request_result_cats_in_search_field() throws Throwable {
+        WebElement searchResult = driver.findElement(By.name("q"));
+        assertEquals("Cats", searchResult.getAttribute("value"));
+    }*/
 }
