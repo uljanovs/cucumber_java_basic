4b653cbf43a84f3e9df728d582632e3d32556345
g.balcere <gbalcere@gmail.com>
2020-03-06 09:12:49 +0200

    Google activity

--- src/test/java/stepDefinitions/Activity_2Steps.java
+++ src/test/java/stepDefinitions/Activity_2Steps.java
@@ -6,6 +6,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import pages_sample.Activity_2Page;
 
@@ -33,12 +34,12 @@ public class Activity_2Steps {
 
    @And("^I click Search button$")
          public void clickSearch() throws Throwable {
-            driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div[1]/div[3]/center/input[1]")).click();
+       driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div[1]/div[1]/div/div[2]/input")).sendKeys(Keys.ENTER);
        }
 
     @Then("^result is displayed$")
     public void iSeeResult () throws Throwable {
-        assertTrue(driver.findElement(By.linkText("https://www.google.com/")).isDisplayed());
+        driver.get("https://www.google.com/search?safe=active&sxsrf=ALeKk01SJ5BoMD2lVSLxDi0h3j8Nk1y3Dw%3A1583477217883&source=hp&ei=4fFhXqnCM_KErwSxk7DwAw&q=cats&oq=cats&gs_l=psy-ab.3..35i39j0l8j0i10.1117893.1118534..1350313...4.0..0.96.360.4......0....1..gws-wiz.....10..35i362i39.Gp-Yp7C_qrU&ved=0ahUKEwjpttGooIXoAhVywosKHbEJDD4Q4dUDCAY&uact=5");
     }
 
 }
