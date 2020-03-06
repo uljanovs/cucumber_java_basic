144a477b6d99a123a00149c1fbeaa7a149fc28a7
KonstantsiyaP <kasya.aysak@gmail.com>
2020-03-06 11:20:11 +0200

    GoogleSteps

--- src/test/java/stepDefinitions/GoogleSteps.java
+++ src/test/java/stepDefinitions/GoogleSteps.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.sql.Driver;
 import java.util.List;
 import java.util.Map;
 
@@ -33,20 +34,29 @@ public class GoogleSteps {
 
         @When("^I input cats$")
         public void i_input_cats() throws Throwable {
-            WebElement name = driver.findElement(By.name("q"));
+          WebElement name = driver.findElement(By.name("q"));
             name.sendKeys("Cats");
-        }
+           /*@When("^I input \"([^\"]*)\"$")
+            public void i_input(String item) throws Throwable {
+                driver.findElement(By.name("q")).clear();
+                driver.findElement(By.name("q")).sendKeys(item);*/
+
+            }
+
+
 
         @When("^I click search button$")
         public void i_click_search_button() throws Throwable {
+           // driver.findElement(By.name("btnK")).click();
             WebElement button12 = driver.findElement(By.name("btnK"));
-            button12.click();
+            button12.submit();
         }
 
         @Then("^I see request result cats in search field$")
         public void i_see_request_result_cats_in_search_field() throws Throwable {
             WebElement searchResult = driver.findElement(By.name("q"));
-            assertEquals("Cats", searchResult.getAttribute("value"));
+            assertEquals("cats", searchResult.getAttribute("value"));
+            
         }
 }
 
